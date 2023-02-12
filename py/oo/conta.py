ecb5554a8ebac0ae6bf6505c664c201db9eddec9
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2023-02-12 18:37:11 -0300

    getters and setters

--- py/oo/conta.py
+++ py/oo/conta.py
@@ -2,19 +2,35 @@ class Conta:
 
     def __init__(self, numero,titular,saldo,limite):
         print("construindo objeto...{}".format(self))
-        self.numero=numero
-        self.titular=titular
-        self.saldo=saldo
-        self.limite=limite
-    
+        self.__numero=numero
+        self.__titular=titular
+        self.__saldo=saldo
+        self.__limite=limite
+
     def extrato(self):
-        print("saldo {} do titular {}".format(self.saldo,self.titular))
+        print("saldo {} do titular {}".format(self.__saldo,self.__titular))
 
     def deposita(self, valor):
-        self.saldo+=valor
+        self.__saldo+=valor
 
     def saca(self,valor):
-        self.saldo-=valor
+        self.__saldo-=valor
 
     def extrato(self):
-        return self.saldo
+        return self.__saldo
+
+    def transfere(self,valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite=limite
