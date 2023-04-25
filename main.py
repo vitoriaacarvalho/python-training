9e9cb2be232893362fa271c95467ad96fff8f823
vitoria <vitoria.carvalho@flowsoftware.com>
2023-04-25 10:43:28 -0300

    read method

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ connection=mysql.connector.connect(
 cursor=connection.cursor()
 
 
-#Create
+"""Create
 nome_produto="lindt"
 valor=5
 command=f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}",{valor})'
 cursor.execute(command)
 connection.commit()
+"""
+
+#Read
+get_command= f'SELECT * FROM vendas'
+cursor.execute(get_command)
+result=cursor.fetchall()
+print(result)
+
+
 """
 cursor=connection.cursor()
 command=''   #aspas simples sempre nas queries
@@ -23,12 +32,6 @@ cursor.execute(command)    #isso aqui é obrigatorio em todos os metodos de crud
 connection.commit()    #isso é usado APENAS em métodos que alteram o db. O get não tem isso
 consulta=cursor.fetchall()     #para ler o banco de dados
 """
-
-
-
-
-
-
 
 
 cursor.close()
