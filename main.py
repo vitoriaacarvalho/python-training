9e64d1063d87ee365ec2f79cba5d507625c97b6e
vitoria <vitoria.carvalho@flowsoftware.com>
2023-04-25 10:34:04 -0300

    create method

---
+++ main.py
@@ -0,0 +1,35 @@
+import mysql.connector
+connection=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    database='dbyoutube'
+)
+
+
+cursor=connection.cursor()
+
+
+#Create
+nome_produto="lindt"
+valor=5
+command=f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}",{valor})'
+cursor.execute(command)
+connection.commit()
+"""
+cursor=connection.cursor()
+command=''   #aspas simples sempre nas queries
+cursor.execute(command)    #isso aqui é obrigatorio em todos os metodos de crud
+connection.commit()    #isso é usado APENAS em métodos que alteram o db. O get não tem isso
+consulta=cursor.fetchall()     #para ler o banco de dados
+"""
+
+
+
+
+
+
+
+
+cursor.close()
+connection.close()
